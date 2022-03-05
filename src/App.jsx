12bd1155024ab8844a016badd7754c556eb15f5f
303321165@qq.com <303321165@qq.com>
2022-03-05 08:45:28 +0800

    TIJIAO

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,23 @@
 import React, { Component } from 'react'
-import {nanoid} from 'nanoid'
-import Header from './Header/header'
-import List   from './List/list'
-import Footer from './Footer/footer'
 import './App.css'
+import axios  from 'axios'
+export default  class App extends Component{
 
-export default class App extends Component {
-  state = {todos:[
-    {"id":1,"name":"吃饭",done:false},
-    {"id":2,"name":"睡觉",done:false},
-    {"id":3,"name":"写代码",done:true},
-    {"id":4,"name":"洗澡",done:true}
-  ]}
-
-  addTodo = (todoObj)=>{
-    const {todos} = this.state
-    const newTodos = [todoObj,...todos]
-    this.setState({todos:newTodos})
-  }
-  updateTodo =(id,done)=>{
-    const {todos} = this.state
-    let newtodos = todos.map(m=>{
-      if(m.id === id) return {...m,done}
-      else{
-        return m;
-      }
+  getStudentData = ()=>{
+    axios.defaults.baseURL = 'http://localhost:5000';
+    axios({
+      method:"GET",
+      url:'/posts'
+    }).then((response)=>{
+      console.log(response);
+    },(error)=>{
+      console.log(error);
     })
-    this.setState({todos:newtodos})
   }
-  delTodo = (id)=>{
-    const {todos} = this.state
-    let newtodos = todos.filter(m=>m.id!=id);
-    this.setState({todos:newtodos})
-
-  }
-  checkAll = (flag)=>{
-    const {todos} = this.state;
-    const newtodos =todos.map(m=>{
-      return {...m,done:flag}
-    });
-    
-    this.setState({todos:newtodos})
-  }
-  clearHadCheck =()=>{
-    const {todos} = this.state;
-    const newtodos = todos.filter(m=>!m.done)
-    this.setState({todos:newtodos})
-  }
-
-  render() {
-    return (
-      <div className='todo-container'>
-        <div className='todo-wrap'>
-          <Header addTodo ={this.addTodo} />
-          <List todos={this.state.todos} updateTodo={this.updateTodo} delTodo={this.delTodo}/>
-          <Footer todos={this.state.todos} checkAll= {this.checkAll}  clearHadCheck={this.clearHadCheck}  />
-        </div>
-         
-      </div>
-      
-    )
+  render(){
+    return (  
+    <div>
+      <button onClick={this.getStudentData}> 点我获取学生数据 </button>
+    </div>)
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import  Helloreact  from'./Helloreact/Helloreact'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
